54c922e671caf2daf97b49aa10e74fd8646f41b9
jaewon496 <jaewon496@gmail.com>
2026-01-29 12:48:56 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const hideLoading = () => {
-  const loading = document.getElementById('initial-loading');
-  if (loading) {
-    loading.classList.add('hidden');
-    setTimeout(() => loading.remove(), 300);
-  }
-};
+// React를 전역으로 노출 (일부 라이브러리 호환성)
+window.React = React
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-hideLoading();
