24b3ef227ba668c874e87d5a0c949a81f4568fd4
jaewon496 <jaewon496@gmail.com>
2026-01-29 13:32:56 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// React를 전역으로 노출 (일부 라이브러리 호환성)
-window.React = React
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
